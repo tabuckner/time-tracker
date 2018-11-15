e4678e44b74ccc63cf79b6c797d69846b4e44c78
Taylor Buckner <tbuckner@theappraisallane.com>
2018-11-15 17:48:23 -0600

    Auth Service cleanup.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,38 +1,14 @@
 import { Injectable } from '@angular/core';
-// import { Router } from '@angular/router';
-
-import { auth, } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-
-import { Observable, of , Subject} from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-
-interface User {
-  uid: string;
-  email: string;
-  photoURL?: string;
-  displayName?: string;
-  favoriteColor?: string;
-}
+import { Observable, Subject} from 'rxjs';
 
 @Injectable()
 export class AuthService {
   userId: string;
   userIdUpdated = new Subject<string>();
-  user: Observable<User>;
 
-  constructor(private afAuth: AngularFireAuth,
-    private afs: AngularFirestore) {
-    // this.user = this.afAuth.authState.pipe(
-    //   switchMap(user => {
-    //     if (user) {
-    //       return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
-    //     } else {
-    //       return of(null);
-    //     }
-    //   })
-    // );
+  constructor(private afAuth: AngularFireAuth) {
   }
 
   public setUserId(id: string) {
@@ -53,34 +29,9 @@ export class AuthService {
       .catch((e) => { console.error(e); });
   }
 
-  public anonymousLoginHandler(a: auth.UserCredential) {
+  public anonymousLoginHandler(a) {
     this.setUserId(a.user.uid);
     this.userIdUpdated.next(this.getUserId());
-  }
-
-  public googleLogin() {
-    const provider = new auth.GoogleAuthProvider();
-    return this.oAuthLogin(provider);
-  }
-
-  private oAuthLogin(provider) {
-    return this.afAuth.auth.signInWithPopup(provider)
-      .then((credential) => {
-        this.updateUserData(credential.user);
-      });
-  }
-
-  private updateUserData(user) {
-    // Sets user data to firestore on login
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
-    const data: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL
-    };
-
-    return userRef.set(data, { merge: true });
   }
 
   signOut() {
