feea790b8cc7eed20f69d167cd0657237c4687ea
Taylor Buckner <tbuckner@theappraisallane.com>
2018-11-15 11:57:36 -0600

    Added script to build for github pages on master.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAdrTmgNDZVWS1eWGr3x-ZlC1J5uXd1GqA',
+    authDomain: 'time-tracker-a87b5.firebaseapp.com',
+    databaseURL: 'https://time-tracker-a87b5.firebaseio.com',
+    projectId: 'time-tracker-a87b5',
+    storageBucket: 'time-tracker-a87b5.appspot.com',
+    messagingSenderId: '969017736901'
+  }
 };
